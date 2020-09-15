fe844e70c79651d796b4bdca0cedb5b73854acc9
Jae <45396620+workhard1010@users.noreply.github.com>
2020-09-15 10:56:29 +0800

    Update main.cpp

--- 数组中常见的问题/80. 删除排序数组中的重复项 II/main.cpp
+++ 数组中常见的问题/80. 删除排序数组中的重复项 II/main.cpp
@@ -1,23 +1,24 @@
 //==================================================================
-// Leetcode����
-// ���ߣ��ܼ�
+// Leetcode代码
+// 作者：曹佳
 //==================================================================
 
-// 80. ɾ�����������е��ظ��� II
-// ��Ŀ������һ���������飬����Ҫ��ԭ��ɾ���ظ����ֵ�Ԫ�أ�ʹ��ÿ��Ԫ�����������Σ������Ƴ���������³��ȡ�
-// ��Ҫʹ�ö��������ռ䣬�������ԭ���޸��������鲢��ʹ�� O(1) ����ռ����������ɡ�
+// 80. 删除排序数组中的重复项 II
+// 题目：给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+// 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-//����ⷨ�����ƹ㵽����n����ͬԪ��
+//这个解法可以推广到保留n个相同元素
+//双指针，快慢指针
 
 int removeDuplicates(vector<int>& nums) {
     int slowIndex = 0;
     for( int fastIndex = 0; fastIndex < nums.size(); fastIndex++)
         if( slowIndex < 2 || nums[fastIndex] != nums[slowIndex-2] )
-            if(slowIndex == fastIndex)  //�������ָ����ͬ����ָ���ֱ��ǰ�������ø��ƣ��ķ�ʱ��
+            if(slowIndex == fastIndex)  //如果快慢指针相同，慢指针可直接前进，不用复制，耗费时间
                 slowIndex++;
             else
                 nums[slowIndex++] = nums[fastIndex];
