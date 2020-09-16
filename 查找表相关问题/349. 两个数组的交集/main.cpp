716ef2bd55209006c957ccb12cd1ec14a98775aa
Jae <335181140@qq.com>
2020-09-16 18:12:17 +0800

    Update main.cpp

--- 查找表相关问题/349. 两个数组的交集/main.cpp
+++ 查找表相关问题/349. 两个数组的交集/main.cpp
@@ -21,7 +21,7 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
             if( record.find(nums2[i]) != record.end())
                 resultSet.insert(nums2[i]);
         }
-        return vector<int>(resultSet.begin(), resultSet.end());
+        return {resultSet.begin(), resultSet.end()}; // C++ 新标准 函数返回值列表初始化
     }
 
 int main()
