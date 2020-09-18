c7dba3f736529a9de89c01652d7804c51c3ff001
Jae <335181140@qq.com>
2020-09-18 22:00:12 +0800

    Update main.cpp

--- 查找表相关问题/349. 两个数组的交集/main.cpp
+++ 查找表相关问题/349. 两个数组的交集/main.cpp
@@ -17,9 +17,9 @@ using namespace std;
 vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         set<int> record(nums1.begin(), nums1.end());
         set<int> resultSet;
-        for(int i = 0; i < nums2.size(); i++){
-            if( record.find(nums2[i]) != record.end())
-                resultSet.insert(nums2[i]);
+        for(vector<int>::iterator it = nums2.begin(); it != nums2.end(); ++it){
+            if( record.find(*it) != record.end())
+                resultSet.insert(*it);
         }
         return {resultSet.begin(), resultSet.end()}; // C++ 新标准 函数返回值列表初始化
     }
