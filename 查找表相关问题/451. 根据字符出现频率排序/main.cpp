824b9154cb7d3e3154f95cbf4b052a60ae53bae2
Jae <335181140@qq.com>
2020-09-23 14:59:16 +0800

    Update main.cpp

--- 查找表相关问题/451. 根据字符出现频率排序/main.cpp
+++ 查找表相关问题/451. 根据字符出现频率排序/main.cpp
@@ -12,9 +12,12 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <queue>
+#include <string>
 
 using namespace std;
 
+//数组记录频率
 string frequencySort(string s) {
     int arr[256] = {0};
     for(int i = 0; i < s.size(); ++i){
@@ -32,10 +35,29 @@ string frequencySort(string s) {
     return str;
 }
 
+//优先队列
+string frequencySort1(string s) {
+    unordered_map<char, int> record;
+    for(const auto &i : s)
+        ++record[i];
+    priority_queue<pair<char, int>> pq;
+    for(const auto &i : record)
+        pq.push(make_pair(i.second, i.first));
+    string res;
+    while(!pq.empty()){
+        auto t = pq.top();
+        pq.pop();
+        //while(t.first--)
+        //res+=t.second;//res.push_back(t.second_;
+        res.append(t.first, t.second);
+    }
+    return res;
+}
+
 int main()
 {
     string str = "tree";
-    string st = frequencySort(str);
+    string st = frequencySort1(str);
     cout << st << endl;
     return 0;
 }
