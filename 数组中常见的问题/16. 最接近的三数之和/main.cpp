00cbeea5c7eef2e7564018bad21420a2c312616e
Jae <335181140@qq.com>
2020-09-25 16:28:16 +0800

    Update main.cpp

--- 数组中常见的问题/16. 最接近的三数之和/main.cpp
+++ 数组中常见的问题/16. 最接近的三数之和/main.cpp
@@ -20,29 +20,41 @@ using namespace std;
 int threeSumClosest(vector<int>& nums, int target) {
     sort(nums.begin(), nums.end());
     int res1 = 1e7;
-    int res2 = -1e7;
+    //int res2 = -1e7;
     if(nums.size() < 3)
         return 0;
+    auto update = [&](int cur){
+        if(abs(cur - target) < abs(res1 - target))
+            res1 = cur;
+    };
     for (int i = 0; i < nums.size()-2; ++i){
         if(i > 0&& nums[i] == nums[i-1])
             continue;
         int left = i+1;
         int right = nums.size() - 1;
         while(left <right){
-            if(nums[i] + nums[left] + nums[right] == target)
+            int sum = nums[i] + nums[left] + nums[right];
+            update(sum);
+            if(sum == target)
                 return target;
-            else if(nums[i] + nums[left] + nums[right] > target){
-                res1 = min(nums[i] + nums[left] + nums[right], res1);
-                --right;}
+            else if(sum > target){
+                //res1 = min(nums[i] + nums[left] + nums[right], res1);
+                --right;
+               // while(left < right&&nums[right] == nums[right-1])
+                   // --right;
+                }
             else{
-                res2 = max(nums[i] + nums[left] + nums[right], res2);
-                ++left;}
+                //res2 = max(nums[i] + nums[left] + nums[right], res2);
+                ++left;
+               // while(left < right&&nums[left] == nums[left+1])
+                   // ++left;
+                    }
         }
 
     }
-        if(abs(res1 - target) > abs(res2 - target))
+        /*if(abs(res1 - target) > abs(res2 - target))
             return res2;
-        else
+        else*/
             return res1;
 
 
@@ -51,8 +63,8 @@ int threeSumClosest(vector<int>& nums, int target) {
 
 int main()
 {
-    vector<int> ivec = {0,2,1,-3};
-    int a = threeSumClosest(ivec, 1);
+    vector<int> ivec = {3,4,5,5,7};
+    int a = threeSumClosest(ivec, 13);
     printf("%d\n", a);
     return 0;
 }
