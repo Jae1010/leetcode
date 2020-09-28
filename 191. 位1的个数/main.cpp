e2c0948131f5d95471707ece9dc0cfa5c99981cb
Jae <335181140@qq.com>
2020-09-28 17:40:48 +0800

    Update main.cpp

--- 191. 位1的个数/main.cpp
+++ 191. 位1的个数/main.cpp
@@ -3,9 +3,9 @@
 // 作者：曹佳
 //==================================================================
 //
-// 
-// 题目：
-// 
+// 191. 位1的个数
+// 题目：编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个
+//      数（也被称为汉明重量）。
 //
 //
 #include <iostream>
@@ -13,8 +13,20 @@
 
 using namespace std;
 
+int hammingWeight(uint32_t n) {
+    int ones = 0;
+    while(n > 0){
+        ones += ( n & 1);
+        n >>= 1;
+    }
+    return ones;
+}
+
 int main()
 {
     vector<int> ivec = { };
+
+    int a = hammingWeight(441);
+    cout << a;
     return 0;
 }
