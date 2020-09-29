c203c232b63f04a244715c6448d30b74fe5791eb
Jae <335181140@qq.com>
2020-09-29 15:57:09 +0800

    Update main.cpp

--- 查找表相关问题/219. 存在重复元素 II/main.cpp
+++ 查找表相关问题/219. 存在重复元素 II/main.cpp
@@ -10,15 +10,47 @@
 //
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
-bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//力扣最后一个用例超时
+bool containsNearbyDuplicate_1(vector<int>& nums, int k) {
+    int left = 0, right = 0;
+    unordered_map<int, int> record;
+    while( right < nums.size()){
+        record[right] = nums[right];
+        for(int i = left; i < right; ++i){
+            if(record[right] == nums[i])
+                return true;
+        }
+        right++;
+        while(right - left > k){
+            left++;
+        }
 
+    }
+    return false;
+}
+
+//滑动窗口，这个窗口由哈希表set维护
+bool containsNearbyDuplicate_2(vector<int>& nums, int k) {
+    unordered_set<int> record;
+    for(int i = 0 ; i < nums.size(); ++i){
+        if(record.find(nums[i]) != record.end())
+            return true;
+        record.insert({nums[i], 1});
+        if(record.size() == k+1)
+            record.erase(nums[i-k]);
+    }
+    return false;
 }
 
 int main()
 {
-    vector<int> ivec = { };
+    vector<int> ivec = {1,2,3,1,2,3};
+    bool a = containsNearbyDuplicate_3(ivec, 2);
+    cout << a;
     return 0;
 }
