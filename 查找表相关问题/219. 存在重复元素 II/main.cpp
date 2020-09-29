88e5e2dfa944081a76186b36edf8d6b350668672
Jae <335181140@qq.com>
2020-09-29 13:43:14 +0800

    Create main.cpp

---
+++ 查找表相关问题/219. 存在重复元素 II/main.cpp
@@ -0,0 +1,24 @@
+//==================================================================
+// Leetcode代码
+// 作者：曹佳
+//==================================================================
+//
+// 219. 存在重复元素 II
+// 题目：给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使
+// 得 nums [i] = nums [j]，并且 i 和 j 的差的 绝对值 至多为 k。
+//
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+}
+
+int main()
+{
+    vector<int> ivec = { };
+    return 0;
+}
