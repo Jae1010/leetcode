bc0df140d96424fffa3f014e100413a60798d517
Jae <45396620+workhard1010@users.noreply.github.com>
2020-11-24 11:48:37 +0800

    Create LinkedList.h

---
+++ 在链表中穿针引线/206. 反转链表/LinkedList.h
@@ -0,0 +1,49 @@
+#ifndef __LINKEFLIST_H__
+#define __LINKEFLIST_H__
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+//Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+
+ ListNode* creatLinkedList(int arr[], int n){
+
+    if(n == 0)
+        return NULL;
+
+    ListNode* head = new ListNode(arr[0]);
+
+    ListNode* curHead = head;
+
+    for(int i = 1; i < n; i++){
+        curHead->next = new ListNode(arr[i]);
+        curHead = curHead->next;
+    }
+
+    return head;
+ }
+
+void printLinkedList(ListNode *head){
+
+    ListNode* curHead = head;
+
+    while(curHead != NULL){
+        cout << curHead->val <<" -> ";
+        curHead = curHead->next;
+    }
+    cout << "NULL"<< endl;
+
+    return;
+}
+
+
+#endif // __LINKEFLIST_H__
